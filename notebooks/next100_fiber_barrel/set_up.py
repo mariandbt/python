e086fcee0519d4584818d10d231df0c0c000343a
marian <mariandelbarrio@gmail.com>
2024-02-06 15:47:41 +0100

    Update 20240206@1547

---
+++ notebooks/next100_fiber_barrel/set_up.py
@@ -0,0 +1,44 @@
+
+# Preparing the data
+
+import numpy              as np
+import pandas             as pd
+import tables             as tb
+
+import os
+import h5py
+
+def check_h5(file_path):
+    """
+    Check an HDF5 file is not empty and see it's tables using tb.open_file().
+
+    Parameters:
+    - file_path (str): The path to the HDF5 file.
+
+    Returns:
+    - DataFrame: The loaded DataFrame.
+    """
+
+    with tb.open_file(file_path) as file:
+        print(file)
+
+
+def read_sens_h5(file_path):
+    """
+    Read an HDF5 file using pd.read_hdf() with an optional condition.
+
+    Parameters:
+    - file_path (str): The path to the HDF5 file.
+
+    Returns:
+    - DataFrame: The loaded DataFrame.
+    """
+
+    sns_positions = pd.read_hdf(file_path, "/MC/sns_positions", where='sensor_name == F_SENSOR')
+    sns_response = pd.read_hdf(file_path, "/MC/sns_response")
+
+
+    sns_response = sns_response.loc[sns_response.sensor_id.isin(sns_positions.sensor_id)] # get the positions of said sensors
+
+
+    return sns_positions, sns_response
